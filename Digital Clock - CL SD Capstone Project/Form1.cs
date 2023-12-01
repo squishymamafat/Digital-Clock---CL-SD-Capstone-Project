ea9b31b2c19416059ad3df60d669d53b0203c804
Josh Norman <135571751+squishymamafat@users.noreply.github.com>
2023-12-01 09:13:48 -0500

    Add files via upload
    Initial Upload

---
+++ Digital Clock - CL SD Capstone Project/Form1.cs
@@ -0,0 +1,15 @@
+namespace Digital_Clock___CL_SD_Capstone_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
